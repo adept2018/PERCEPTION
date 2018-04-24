f600778241d53cbe536652b73e28a52db48c679a
Alexander Hemberg <alexander.hemberg@afconsult.com>
2018-04-24 18:39:03 +0200

    Small bugfix in BBox main.py
    looks for .jpeg instead of .JPEG

--- BBox-Label-Tool-master/main.py
+++ BBox-Label-Tool-master/main.py
@@ -131,7 +131,7 @@ class LabelTool():
 ##            return
         # get image list
         self.imageDir = os.path.join(r'./Images', '%03d' %(self.category))
-        self.imageList = glob.glob(os.path.join(self.imageDir, '*.JPEG'))
+        self.imageList = glob.glob(os.path.join(self.imageDir, '*.jpeg'))
         if len(self.imageList) == 0:
             print 'No .JPEG images found in the specified dir!'
             return
